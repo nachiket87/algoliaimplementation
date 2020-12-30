39ba5fb3693297290c64055449306c04c76c6634
Nachiket Pusalkar <nachiket87@gmail.com>
2020-12-30 18:48:21 -0500

    added some styling

--- app/javascript/packs/components/App.jsx
+++ app/javascript/packs/components/App.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import fetchData from "../helpers/fetchData.js";
 import { Input } from "reactstrap";
+import { useDebouncedCallback } from "use-debounce";
 
 const App = () => {
   const [movies, setMovies] = useState();
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("Star Wars");
 
   useEffect(() => {
     fetchData(searchTerm, setMovies);
@@ -16,25 +17,27 @@ const App = () => {
     rnMovie = movies.data.map((movie) => {
       return (
         <li className={"list-group-item"} key={movie.attributes.objectid}>
-          {movie.attributes.title}
+          {movie.attributes.title} ({movie.attributes.year})
         </li>
       );
     });
   }
 
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
+  const debounced = useDebouncedCallback((value) => {
+    setSearchTerm(value.trim());
+  }, 1000);
 
   return (
     <div className="container my-3">
-      <div className="row">
-        <div className="col" style={{ marginBottom: "3px" }}>
+      <div className="row d-flex">
+        <div className="col lg-12">
           <Input
-            onChange={(e) => handleChange(e)}
+            onChange={(e) => debounced.callback(e.target.value)}
             placeholder={`Search for a Movie`}
           />
         </div>
+      </div>
+      <div className="row my-2">
         <div className="col">
           <ul className={"list-group"}>{rnMovie}</ul>
         </div>
