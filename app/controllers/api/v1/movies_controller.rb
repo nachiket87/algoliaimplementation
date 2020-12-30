0d69560087d3c02c8762af5ae5866e603c1c70fc
Nachiket Pusalkar <nachiket87@gmail.com>
2020-12-30 15:34:08 -0500

    post req for movie and clean up

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class MoviesController < ApplicationController
+      protect_from_forgery with: :null_session
       def index
         @movies = Movie.first(1)
         render json: MovieSerializer.new(@movies, options).serializable_hash.to_json
@@ -11,10 +12,24 @@ module Api
         render json: MovieSerializer.new(@movie, options).serializable_hash.to_json
       end
 
+      def create
+        movie = Movie.new(movie_params)
+
+        if movie.save
+          render json: MovieSerializer.new(movie, options).serializable_hash.to_json
+        else
+          render json: { error: movie.errors.message }, status: 422
+        end
+      end
+
       private
 
       def options
-        @options ||= { include: %i[actors genres] }
+        @options ||= { include: %i[actors genres alternate_names] }
+      end
+
+      def movie_params
+        params.require(:movie).permit(:title, :year, :rating, :color)
       end
 
     end
