2a05c17592599fd7c08b1a659749db188f2a30b5
Nachiket Pusalkar <nachiket87@gmail.com>
2020-12-31 01:22:04 -0500

    deployed version 1

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,6 +1,6 @@
 AlgoliaSearch.configuration = { 
-  application_id: ENV['appID'], 
-  api_key: ENV['adminAPI'],
+  application_id: '6XU8A3UQ7F', 
+  api_key: 'c82364b5d1d8961392c091ead329f573',
   connect_timeout: 2,
   receive_timeout: 30,
   send_timeout: 30,
