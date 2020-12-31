b0681c56662e0247f2c88e77c8dc3e41474a431a
Nachiket Pusalkar <nachiket87@gmail.com>
2020-12-31 12:38:51 -0500

    new

--- config/initializers/algoliasearch2.rb
+++ config/initializers/algoliasearch2.rb
@@ -2,8 +2,8 @@ appid = ENV['appID']
 apikey = ENV['adminAPI'] 
 
 AlgoliaSearch.configuration = { 
-  application_id: appid, 
-  api_key: apikey,
+  application_id: '6XU8A3UQ7F', 
+  api_key: 'c82364b5d1d8961392c091ead329f573',
   connect_timeout: 2,
   receive_timeout: 30,
   send_timeout: 30,
